84d6630a37cca36767354e439ba6515f5bdfcf5d
AnuTech-01 <anujangid2902@gmail.com>
2024-12-01 09:38:35 -0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+#Argument
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("rahul")
+my_function("ravi")
+my_function("Lucky")
+
+
+#This function expects 2 arguments, and gets 2 arguments:
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("rahul", "Ravi")
