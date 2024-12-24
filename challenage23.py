5e92bf101eac4f2a3327f6d663ed7328bf8cf41d
Anu jangir <anujangid2902@gmail.com>
2024-12-23 20:59:27 -0800

    Add files via upload

---
+++ challenage23.py
@@ -0,0 +1,25 @@
+#CHALLENAGE 23
+
+#Python String Formation
+
+# 1. Using f-string formatting
+name = "Anu jangid"
+age = 19
+greeting = f"Hello, my name is {name} and I am {age} years old."
+print(greeting)
+
+# 2. Using str.format() method
+name = "divya"
+age = 19
+greeting = "Hello, my name is {} and I am {} years old.".format(name, age)
+print(greeting)
+
+# 3. Using % formatting
+name = "madhu sharma"
+age = 20
+greeting = "Hello, my name is %s and I am %d years old." % (name, age)
+print(greeting)
+
+
+
+
