120fa73e76be754681b3b095b577fb5369da26a8
Anu jangir <anujangid2902@gmail.com>
2024-12-27 22:32:05 -0800

    Add files via upload

---
+++ challenage27.py
@@ -0,0 +1,11 @@
+#CHALLENAGE 27
+
+#Python Read Files
+
+# Open a file in read mode
+with open('example.txt', 'r') as file:
+    # Read the content of the file
+    content = file.read()
+
+# Print the content
+print(content)
